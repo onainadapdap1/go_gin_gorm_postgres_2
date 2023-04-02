1de6232bbe37bc7da316f21887dcf6521d823495
Onai Nadapdap <onainadapdap1@gmail.com>
2023-04-02 14:42:26 +0700

    initial commit

--- models/book.go
+++ models/book.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type Book struct {
-	ID uint `gorm:"primaryKey"`
-	Name string `gorm:"not null;type:varchar(191)" binding:"required"`
-	Author string `gorm:"not null;type:varchar(191)" binding:"required"`
+	ID uint `json:"id" gorm:"primaryKey"`
+	Name string `json:"name_book" gorm:"not null;type:varchar(191)" binding:"required"`
+	Author string `json:"author" gorm:"not null;type:varchar(191)" binding:"required"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
